b10b9a8f374627e5e26b2dddd1d8bee54e2408ef
amesaros2793 <51731819+amesaros2793@users.noreply.github.com>
2019-07-01 22:49:47 -0500

    Update Coordinate.cs

--- Coordinate.cs
+++ Coordinate.cs
@@ -9,6 +9,10 @@ public class Coordinate
     private int row;
     private int col;
 
+    //*******************************************************
+
+    // constructors
+    
     // no argument constructor
     public Coordinate()
     {
