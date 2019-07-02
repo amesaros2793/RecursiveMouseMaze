8af430a2579d68a7ea51b268178a92dab2aafb6f
amesaros2793 <51731819+amesaros2793@users.noreply.github.com>
2019-07-01 21:31:32 -0500

    Add files via upload

---
+++ CheeseBehavior.cs
@@ -0,0 +1,15 @@
+﻿/********************************************************
+* CheeseBehavior.cs
+* Austen Mesaros
+*
+* This class rotates the cheese
+*********************************************************/
+using UnityEngine;
+
+public class CheeseBehavior : MonoBehaviour {
+
+    private const float rotationSpeed = .5f;
+
+    // update is called once per frame
+    void Update() => this.transform.Rotate(new Vector3(0, rotationSpeed, 0));
+}
