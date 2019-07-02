4817f5709ba20dcdc34dc1ac4d5c986e3018441a
amesaros2793 <51731819+amesaros2793@users.noreply.github.com>
2019-07-01 22:49:15 -0500

    Add files via upload

---
+++ Coordinate.cs
@@ -0,0 +1,46 @@
+﻿/********************************************************
+* Coordinate.cs
+* Austen Mesaros
+*
+* This class defines a coordinate
+*********************************************************/
+public class Coordinate
+{
+    private int row;
+    private int col;
+
+    // no argument constructor
+    public Coordinate()
+    {
+        this.row = 0;
+        this.col = 0;
+    }
+
+    // two argument constructor
+    public Coordinate(int row, int col)
+    {
+        this.row = row;
+        this.col = col;
+    }
+
+    // copy constructor
+    public Coordinate(Coordinate coordinate)
+    {
+        this.row = coordinate.row;
+        this.col = coordinate.col;
+    }
+
+    //*******************************************************
+
+    // accessors
+
+    public int getRow()
+    {
+        return row;
+    }
+
+    public int getCol()
+    {
+        return col;
+    }
+}
